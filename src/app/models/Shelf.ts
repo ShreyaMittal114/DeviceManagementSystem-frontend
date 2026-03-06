bec3ccabb60fb7c91e10965645c87471201f8fd7
Mittal <shmittal@ciena.com>
2026-03-07 01:10:20 +0530

    Changes in shelf summary

--- src/app/models/Shelf.ts
+++ src/app/models/Shelf.ts
@@ -1,7 +1,13 @@
+import { Device } from "./device";
+import { ShelfPosition } from "./ShelfPosition";
+
 export interface Shelf{
     id?:string;
     name:string;
     partNumber:string;
     Deleted?:boolean;
     Occupied?:boolean;
+    shelfPosition? :ShelfPosition;
+    device?:Device;
+
 }
